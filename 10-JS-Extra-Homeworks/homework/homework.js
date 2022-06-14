dce2faac04aa1118c4fe1e13a933e66990f597b3
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-06-14 15:05:34 -0500

    A medias

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -50,6 +50,18 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var delabc = "";
+  for(var i = 0; i < cadena.length; i++)
+  {
+    if(cadena[i] === "a" || cadena[i] === "b" || cadena[i] === "c")
+    {
+      continue;
+    }else
+    {
+      delabc += cadena[i];
+    }
+  }
+  return delabc;
 }
 
 
@@ -57,6 +69,20 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  let contador = 0;
+  while(contador < arr.length)
+  {
+    for(var i = 0; i < arr.length - 1; i++)
+    {
+      if(arr[i].length > arr[i+1].length){
+      let Save = arr[i];
+      arr[i] = arr[i+1];
+      arr[i+1] = Save;
+      }
+    }
+    contador++;
+  }
+  return arr;
 }
 
 
@@ -66,6 +92,18 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var organizado = [];
+  for (var i = 0; i < arreglo1.length; i++)
+  {
+    for(var j = 0; j < arreglo2.length; j++)
+    {
+      if(arreglo1[i] === arreglo2[j])
+      {
+        organizado.push(arreglo1[i]);
+      }
+    }
+  }
+  return organizado;
 }
 
 
